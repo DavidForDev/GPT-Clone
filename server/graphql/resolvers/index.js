5f4bc76fd3f805328887bac40da183f3c1978121
DavidForDev <dato.takidze@geolab.edu.ge>
2024-03-17 18:52:41 +0400

    GPT-26

--- server/graphql/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_resolver_1 = __importDefault(require("./user.resolver"));
-const chat_resolver_1 = __importDefault(require("./chat.resolver"));
-exports.default = [user_resolver_1.default, chat_resolver_1.default];
