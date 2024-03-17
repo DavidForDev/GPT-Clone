cb3aad0922ef4144656af05766b3ce51a68f065c
DavidForDev <dato.takidze@geolab.edu.ge>
2024-03-17 17:46:34 +0400

    GPT-22

--- server/dist/db/doc/chats.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fireBase_1 = __importDefault(require("../fireBase"));
-exports.default = fireBase_1.default.firestore().collection("chats");
