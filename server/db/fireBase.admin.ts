1a052c044d930064fb7ba5786c5a73ced60d1f21
DavidForDev <dato.takidze@geolab.edu.ge>
2024-03-18 18:06:33 +0400

    GPT-49

--- server/db/fireBase.admin.ts
+++ server/db/fireBase.admin.ts
@@ -4,7 +4,9 @@ const fireBase_Admin = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY as string,
+    privateKey: process.env.PRIVATE_KEY
+      ? JSON.parse(process.env.PRIVATE_KEY)
+      : undefined,
   }),
   databaseURL: process.env.DATA_BASE_URL,
 });
