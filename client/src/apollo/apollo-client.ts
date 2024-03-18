ae171a41dd76c2cd9fffc72e3f9a68e7e89925f7
DavidForDev <dato.takidze@geolab.edu.ge>
2024-03-18 23:17:34 +0400

    GPT-55

--- client/src/apollo/apollo-client.ts
+++ client/src/apollo/apollo-client.ts
@@ -1,14 +1,14 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "https://gpt-clone-gamma.vercel.app/",
+  uri: process.env.SERVER_SIDE_URL,
   credentials: "include",
 });
 
 const Apollo = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  uri: "https://gpt-clone-gamma.vercel.app/",
+  uri: process.env.SERVER_SIDE_URL,
 });
 
 export default Apollo;
