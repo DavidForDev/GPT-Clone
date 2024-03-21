6e5d7374f6246fd69d33c3e22fd723e9557b0b87
DavidForDev <dato.takidze@geolab.edu.ge>
2024-03-21 18:52:51 +0400

    GPT-62

--- client/src/apollo/apollo-client.ts
+++ client/src/apollo/apollo-client.ts
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_SERVER_SIDE_URL,
   credentials: "include",
+  fetchOptions: {
+    mode: "cors",
+  }
 });
 
 const Apollo = new ApolloClient({
