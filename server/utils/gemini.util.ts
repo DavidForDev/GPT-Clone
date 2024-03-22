307914375ea12354a99d78994f8d189758e8e326
DavidForDev <dato.takidze@geolab.edu.ge>
2024-03-22 11:49:54 +0400

    GPT-74

--- server/utils/gemini.util.ts
+++ server/utils/gemini.util.ts
@@ -13,27 +13,36 @@ import { DBHistoryTypes } from "../types/utils/gemini";
 */
 
 export const useChat = async (dbHistory: DBHistoryTypes[], message: string) => {
-  const chat = model.startChat({
-    history: dbHistory,
-  });
+  try {
+    const chat = model.startChat({
+      history: dbHistory,
+    });
 
-  const result = await chat.sendMessage(message);
-  const response = result.response;
+    const result = await chat.sendMessage(message);
+    const response = result.response;
 
-  return {
-    response: response.text(),
-  };
+    return {
+      response: response.text(),
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const useGenerateAI = async (introduction: string, message: string) => {
   const intro = introduction ? introduction : "";
+  try {
+    const generatedText = await model.generateContent(
+      `${intro} " ${message} "`
+    );
+    const response = generatedText.response;
 
-  const generatedText = await model.generateContent(`${intro} " ${message} "`);
-  const response = generatedText.response;
-
-  return {
-    text: response.text(),
-  };
+    return {
+      text: response.text(),
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 // --- Message Response
